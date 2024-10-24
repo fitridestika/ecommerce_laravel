a009f387fe0bf1b794fbd095bd48924efcac90dd
fitridestika <fitridestika22@gmail.com>
2024-10-24 10:59:11 +0700

    First Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,32 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use App\Models\Skripsi;
-
-class SkripsiSeeder extends Seeder
-{
-    public function run()
-    {
-        Skripsi::insert([
-            [
-                'judul' => 'Analisis Sistem Informasi',
-                'nama' => 'Nuryanti',
-                'nim' => '6304221512',
-                'angkatan' => 2022,
-                'dosen_pembimbing1' => 'Fajri Profesio Putra, M.Kom',
-                'dosen_pembimbing2' => 'Elvi Rahmi, M.Kom'
-            ],
-            
-            // Tambahkan lebih banyak data jika diperlukan
-        ]);
-    }
-}
-
-/*namespace Database\Seeders;
-
 use App\Models\User;
 use App\Models\Admin;
+use App\Models\Skripsi;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -36,14 +13,14 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    /*public function run(): void
+    public function run(): void
     {
         // User::factory(10)->create();
 
         User::create([
             'name' => 'user1',
-            'email' => 'user@gmail.com',
-            'password' => bcrypt('123456789'),
+            'email' => 'user1@gmail.com',
+            'password' => bcrypt('12345678910'),
             'point' => 10000,
         ]);
 
@@ -52,7 +29,14 @@ class DatabaseSeeder extends Seeder
             'username' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('123456789'),
-            
+        ]);
+        Skripsi::create([
+                'judul' => 'Manajemen Proyek Perangkat Lunak',
+                'nama' => 'Fitri Destika',
+                'nim' => '6304221485',
+                'angkatan' => 2022,
+                'dosen_pembimbing1' => 'Fajri Profesio Putra, M.Kom',
+                'dosen_pembimbing2' => 'Elvi Rahmi, M.Kom'
         ]);
     }
-}*/
+}
