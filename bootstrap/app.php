c2ab642167262b2fc0a4647a035f875b2d1db011
fitridestika <fitridestika22@gmail.com>
2024-10-24 10:55:45 +0700

    First Commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'admin' => App\Http\Middleware\AdminMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // ...
     })->create();
